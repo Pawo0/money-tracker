3a0be829f48464bc95912bbef28cf5233d72fabc
PawoLin <czajczykp@gmail.com>
2025-09-20 22:59:08 +0200

    pass env data to Google provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { GoogleButton } from "@/components/Button/index";
-import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
-  const { data: session } = useSession();
-  console.log("Session data:");
-  console.log(session);
   return (
     <div>
-      <GoogleButton>Sign In via Google</GoogleButton>
+      <GoogleButton />
     </div>
   )
 }
--- src/components/Button/GoogleButton.tsx
+++ src/components/Button/GoogleButton.tsx
@@ -1,12 +1,20 @@
 "use client"
 
 import Button from "./Button";
-import {signIn} from "next-auth/react";
+import {signIn, signOut} from "next-auth/react";
+import {useSession} from "next-auth/react";
 
-export default function GoogleButton({children}: {children: React.ReactNode}) {
+export default function GoogleButton() {
+  const {data: session} = useSession();
+
   return (
+    !session ?
     <Button onClick={() => signIn("google")}>
-      {children}
+      Sign In with Google
+    </Button> :
+    <Button onClick={() => signOut()}>
+      Sign Out
     </Button>
+
   )
 }
