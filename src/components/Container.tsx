9242b16bb229e9d8f6f87d1f48666d009f6a0941
PawoLin <czajczykp@gmail.com>
2025-10-02 19:51:42 +0200

    updated screen sizing

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -16,13 +16,13 @@ function Container({
   return (
     <section
       className={cn(
-        "bg-black/70 rounded-2xl min-h-screen flex flex-col mx-auto px-4 sm:px-6 lg:px-8",
+        "bg-black/70 rounded-2xl min-h-screen flex flex-col mx-auto px-4 sm:px-6 lg:px-8 ",
         {
           'max-w-sm': size === 'sm',
           'max-w-md': size === 'md',
           'max-w-xl': size === 'xl',
           'max-w-none': size === 'full',
-          'max-w-sm sm:max-w-md md:max-w-xl xl:max-w-2xl': size === 'auto',
+          'sm:max-w-xl xl:max-w-2xl': size === 'auto',
         },
         className
       )}
