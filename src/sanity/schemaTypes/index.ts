8c6da6129c9528b3537bfc62ad1cc05e58584bc0
PawoLin <czajczykp@gmail.com>
2026-01-03 15:21:58 +0100

    removed sanity

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import user from "./user"
-import category from "./category"
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [user, category],
-}
